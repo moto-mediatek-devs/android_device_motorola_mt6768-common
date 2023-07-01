12709ad570361d20b02ff1da557d81b189b62570
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-01 17:26:41 +0700

    mt6768-common: Enforce RRO for all overlays
    Change-Id: Iceb177c9b5322354b8b9f9ce81a91dc04a2350a8

--- mt6768.mk
+++ mt6768.mk
@@ -151,6 +151,9 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/seccomp,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
