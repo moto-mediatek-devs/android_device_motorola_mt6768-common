646248d72ebde28839e18c7130dfe0548155bf34
Matsvei Niaverau <matvej2002@gmail.com>
2023-07-20 19:29:03 +0100

    mt6768-common: Configure Thermal stack
    Change-Id: I1b683695176c62364b58250eedffd2afcc5a633f

--- mt6768.mk
+++ mt6768.mk
@@ -220,6 +220,13 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/mediatek
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl
+
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
