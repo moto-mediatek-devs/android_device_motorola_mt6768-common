84d906c7c6ec5b7328e5d4d5086d0f5ad26f556d
bengris32 <bengris32@protonmail.ch>
2023-07-24 11:01:11 +0100

    mt6768-common: Configure GNSS stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/tree/master/vendor/gnss
    
    Change-Id: I598002e24d64f10a7d7ea8483a1615d925f54e39
    Co-authored-by: electimon <electimon@gmail.com>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -112,6 +112,14 @@ PRODUCT_PACKAGES += \
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss-V1-ndk.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
