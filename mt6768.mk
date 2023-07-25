d02a9ee6b25c0a716a2b818ccc89c1431e9641a2
bengris32 <bengris32@protonmail.ch>
2023-07-25 12:59:56 +0100

    mt6768-common: Configure DRM stack
    Change-Id: I93ba95e0d5174689880ce5b7ab611fe1c2ba5637

--- mt6768.mk
+++ mt6768.mk
@@ -92,6 +92,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
     libdrm.vendor
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
