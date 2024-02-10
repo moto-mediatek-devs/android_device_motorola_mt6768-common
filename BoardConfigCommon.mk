4b7f8dcb0e3184c0cf98e4c5cdf80f29bdbadb6c
bengris32 <bengris32@protonmail.ch>
2024-02-10 14:42:41 +0000

    mt6768-common: Specify kernel DTB build target
    Change-Id: I0cbce8dd6316f5b61e6c2177dbdea8f06c561534
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,9 @@ TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/$(PRODUCT_DEVICE).config
 
+TARGET_KERNEL_DTB := \
+    mediatek/mt6768.dtb
+
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 
 # Partitions
