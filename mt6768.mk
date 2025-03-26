b60ef6036a578ce092d71006cd55a8f96ff7ded0
bengris32 <bengris32@protonmail.ch>
2025-03-26 21:09:33 +0100

    mt6768-common: Build `create_pl_dev` for updating preloader
    Change-Id: I08094318de8308439a367277e210ce5d5de1da51
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -42,6 +42,10 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+PRODUCT_PACKAGES += \
+     create_pl_dev \
+     create_pl_dev.recovery
+
 # Boot Control
 PRODUCT_PACKAGES += \
     com.android.hardware.boot \
