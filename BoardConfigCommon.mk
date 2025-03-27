ffff751e2aebd39a95322428c9f7efe9608660da
alextrack2013 <alextrack2013@gmail.com>
2025-03-27 13:25:00 +0100

    mt6768-common: Include MediaTek FCM
    Change-Id: Ifcfd11c9256f6a94424cc67a62e69f3ffb8fa288

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,7 +68,8 @@ TARGET_NO_BOOTLOADER := true
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 # Kernel
