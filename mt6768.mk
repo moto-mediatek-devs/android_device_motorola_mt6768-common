890c04a5bf2e89ecddee3b70191b2c803f8808ef
R0rt1z2 <me@r0rt1z2.com>
2025-03-29 13:00:05 +0100

    mt6768-common: Configure Camera stack
    Change-Id: I6cba9c3360eccd20ac0c82099203fc225e96b15d

--- mt6768.mk
+++ mt6768.mk
@@ -83,6 +83,11 @@ PRODUCT_PACKAGES += \
     com.android.hardware.boot \
     android.hardware.boot-service.default_recovery
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.6.vendor \
+    android.hardware.camera.provider@2.6.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
@@ -202,7 +207,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-mediatek
+    android.hardware.power-service-mediatek \
+    android.hardware.power@1.3.vendor
 
 PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.0.vendor \
